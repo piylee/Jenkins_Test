cc9f5242810f490e9008a65b0884bd60731aab81
piylee <piylee10@gmail.com>
2023-02-20 11:03:43 +0900

    test test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World! Jenkins auto build test!";
+		return "Hello World! Jenkins auto build test!!!!!!!";
 	}
 
 	public static void main(String[] args) {
